d6034ac4ec7b0dfb065a33a60e75c2dcc1d7a34d
CrookedGrandma <k.t.noot@live.nl>
2018-04-03 23:08:03 +0200

    Result Reader

--- ResultReader/ResultReader/Program.cs
+++ ResultReader/ResultReader/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace ResultReader {
-    class Program {
+namespace SplitImage {
+    static class Program {
+
         static void Main(string[] args) {
+            StreamWriter writer = new StreamWriter("../../out.csv");
+            writer.AutoFlush = true;
+            List<string> files = Directory.GetFiles("../../in", "*.txt").ToList();
+            files.Sort();
+            foreach (string filePath in files) {
+                string[] lines = File.ReadAllLines(filePath);
+                double duration, error;
+                double.TryParse(lines[13].Split()[0].Replace('.', ','), out duration);
+                double.TryParse(lines[15].Split()[0].Replace('.', ','), out error);
+                writer.Write(duration);
+                writer.Write(';');
+                writer.WriteLine(error);
+            }
         }
     }
 }
