32672e6be3329321e99a7ab6c8205729f278889b
CrookedGrandma <k.t.noot@live.nl>
2018-04-04 16:44:09 +0200

    variable floating point precision

--- ResultReader/ResultReader/Program.cs
+++ ResultReader/ResultReader/Program.cs
@@ -6,49 +6,46 @@ using System;
 namespace SplitImage {
     static class Program {
 
+        static int n = 50;
+
         static void Main(string[] args) {
             StreamWriter writer = new StreamWriter("../../out.csv");
             writer.AutoFlush = true;
             List<string> files = Directory.GetFiles("../../in", "*.txt").ToList();
             files.Sort();
-            List<double[]>[] Data = new List<double[]>[100];
+            List<double[]>[] Data = new List<double[]>[50];
             foreach (string filePath in files) {
                 string[] lines = File.ReadAllLines(filePath);
                 double stepSize, error;
-                int steps;
+                int numOfDigits, numOfSteps;
 
-                int.TryParse(lines[6].Split()[0]/*.Replace('.', ',')*/, out steps);
+                //int.TryParse(lines[7].Split()[0].Replace('.', ','), out numOfDigits);
+                int.TryParse(lines[6].Split()[0].Replace('.', ','), out numOfSteps);
                 //double.TryParse(lines[6].Split()[0].Replace('.', ','), out stepSize);
-                double.TryParse(lines[15].Split()[0]/*.Replace('.', ',')*/, out error);
-                if(Data[(steps / 10) - 1] == null)
-                {
-                    Data[(steps / 10) - 1] = new List<double[]>();
+                double.TryParse(lines[15].Split()[0].Replace('.', ','), out error);
+                int index = numOfSteps / 10000 - 1;
+                if (Data[index] == null) {
+                    Data[index] = new List<double[]>();
                 }
-                Data[(steps/10)-1].Add(new double[2]{steps, error});
-
-
-
+                Data[index].Add(new double[2] { numOfSteps, error });
             }
-            foreach(List<double[]> l in Data)
-            {
+            foreach (List<double[]> l in Data) {
                 writer.Write(l[0][0]);
-                writer.Write(',');
+                writer.Write(';');
                 double errorsum = 0;
                 double errorvarianceSum = 0;
-                foreach (double[] d in l)
-                {
+                foreach (double[] d in l) {
                     errorsum += d[1];
                 }
-                double mean = errorsum / 100;
-                foreach (double[] d in l)
-                {
-                    errorvarianceSum += (d[1]-mean)* (d[1] - mean);
+                double mean = errorsum / n;
+                foreach (double[] d in l) {
+                    errorvarianceSum += (d[1] - mean) * (d[1] - mean);
                 }
-                double var = errorvarianceSum / 99;
+                double var = errorvarianceSum / (n - 1);
                 double stDev = Math.Sqrt(var);
-                double standardError = Math.Sqrt(var / 100);
+                double standardError = Math.Sqrt(var / n);
                 writer.Write(mean);
-                writer.Write(',');
+                writer.Write(';');
                 writer.WriteLine(standardError);
             }
         }
